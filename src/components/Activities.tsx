3ca9f9d28aacd74736a85e079b2c58e09c531801
sandygudie <goodnewssandy@gmail.com>
2022-05-25 17:43:12 +0100

    initial commit

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -1,6 +1,6 @@
 function Activities() {
   return (
-    <div className="flex flex-col p-3  pt-11">
+    <div className="flex flex-col p-3 pt-11">
       <div className="h-40 px-2">
         <p className="text-sm font-semibold">INVITES</p>
         <p className="text-xs text-gray-100 pt-2">You have no invites.</p>
@@ -9,8 +9,8 @@ function Activities() {
         <p className="text-sm font-semibold">RECENT PAYMENT</p>
         <p className="text-xs text-gray-100 pt-2">No Payment has been made.</p>
       </div>
-      <div className="h-30 px-2">
-        <p className="text-sm font-semibold pt-3 ">PREVIOUS ORDERS</p>
+      <div className="h-30 px-2 pt-3">
+        <p className="text-sm font-semibold ">PREVIOUS ORDERS</p>
         <p className="text-xs text-gray-100 pt-2">You have created no order.</p>
       </div>
     </div>
